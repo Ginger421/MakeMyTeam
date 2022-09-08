ef32b0171bc000fffad3d3126af4156586cf69ea
Ginger Owen <gingerowen0421@gmail.com>
2022-09-08 11:11:12 -0600

    test for employee added

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,1 +1,15 @@
+const Employee = require("../lib/employee")
 
+//make sure to creat a new Employee maybe using a const at top of file
+
+describe("Employee", ()=> {
+    describe("constructor", ()=> {
+    it('should construct a name, id and email', () => {
+        const employee=newEmployee("Ginger", 9, "test@gmail.com");
+        expect(employee.name).toEqual("Ginger");
+        expect(employee.id).toEqual(9);
+        expect(employee.email).toEqual("test@gmail.com");
+      });
+    });
+    
+}) //ends describe "employee"
