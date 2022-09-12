d530f69fa6a4f1c90fdaa7616a0b4d2a8c29ada0
Ginger Owen <gingerowen0421@gmail.com>
2022-09-12 13:14:39 -0600

    cleaned up tests

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -8,9 +8,6 @@ describe("Employee", ()=> {
     describe("constructor", ()=> {
     it('should construct a name, id and email', () => {
         const employee = new Employee("Ginger", 9, "test@gmail.com");
-        const name = "Ginger";
-        const id = 9;
-        const email = "test@gmail.com";
         expect(employee.name).toEqual("Ginger");
         expect(employee.id).toEqual(9);
         expect(employee.email).toEqual("test@gmail.com");
--- tests/manager.test.js
+++ tests/manager.test.js
@@ -4,10 +4,6 @@ describe("Manager", ()=> {
     describe("constructor", ()=> {
     it('should construct a name, an id, an email and office number', () => {
         const manager = new Manager("Val", 7, "test7@gmail.com", 1);
-        const name = "Val";
-        const id = 7;
-        const email = "test7@gmail.com";
-        const officeNumber = 1;
         expect(manager.name).toEqual("Val");
         expect(manager.id).toEqual(7);
         expect(manager.email).toEqual("test7@gmail.com");
